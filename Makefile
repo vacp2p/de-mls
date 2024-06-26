c14b0a1a9ff29c357c7d35da3a78a1153b3f9464
Aaryamann Challani <43716372+rymnc@users.noreply.github.com>
2024-06-26 15:26:02 +0530

    chore(sc_keystore): add interface of smart contract (#9)
    * fix: makefile
    
    * chore: add interface
    
    * fix: foundry.toml
    
    * fix: add getAvailableKeyPackage

--- Makefile
+++ Makefile
@@ -1,3 +1,12 @@
-deps:
-	# for the contracts
-	git submodule update --init --recursive
+## ref: https://gist.github.com/enil/e4af160c745057809053329df4ba1dc2
+
+GIT=git
+GIT_SUBMODULES=$(shell sed -nE 's/path = +(.+)/\1\/.git/ p' .gitmodules | paste -s -)
+
+.PHONY: deps
+deps: $(GIT_SUBMODULES)
+
+$(GIT_SUBMODULES): %/.git: .gitmodules
+	$(GIT) submodule init
+	$(GIT) submodule update $*
+	@touch $@
---
+++ contracts/src/IScKeystore.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.24;
+
+struct KeyPackage {
+    // store serialized onchain
+    bytes[] data;
+}
+
+struct UserInfo {
+    KeyPackage[] keyPackages;
+    bytes signaturePubKey;
+}
+
+interface IScKeystore {
+    function userExists(address user) external view returns (bool);
+    function addUser(UserInfo calldata userInfo) external;
+    function getUser(address user) external view returns (UserInfo memory);
+    function addKeyPackage(KeyPackage calldata) external;
+    function getAvailableKeyPackage(address user) external view returns (KeyPackage memory);
+}
