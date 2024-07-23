9b23215fb559bcedc32f137c5afebb28eeee1dbe
rymnc <43716372+rymnc@users.noreply.github.com>
2024-07-23 17:28:53 +0530

    fmt

--- contracts/src/ScKeystore.sol
+++ contracts/src/ScKeystore.sol
@@ -10,60 +10,60 @@ error MalformedUserInfo();
 error UserDoesNotExist();
 
 contract ScKeystore is Ownable, IScKeystore {
-  event UserAdded(address user, bytes signaturePubKey);
-  event UserKeyPackageAdded(address indexed user, uint256 index);
+    event UserAdded(address user, bytes signaturePubKey);
+    event UserKeyPackageAdded(address indexed user, uint256 index);
 
-  mapping(address user => UserInfo userInfo) private users;
-  KeyPackage[] private keyPackages;
+    mapping(address user => UserInfo userInfo) private users;
+    KeyPackage[] private keyPackages;
 
-  constructor(address initialOwner) Ownable(initialOwner) {}
+    constructor(address initialOwner) Ownable(initialOwner) { }
 
-  function userExists(address user) public view returns (bool) {
-    return users[user].signaturePubKey.length > 0;
-  }
-
-  function addUser(bytes calldata signaturePubKey, KeyPackage calldata keyPackage) external onlyOwner {
-    if (signaturePubKey.length == 0) revert MalformedUserInfo();
-    if (keyPackage.data.length == 0) revert MalformedKeyPackage();
-    if (userExists(msg.sender)) revert UserAlreadyExists();
-
-    keyPackages.push(keyPackage);
-    uint256 keyPackageIndex = keyPackages.length - 1;
-
-    users[msg.sender] = UserInfo(new uint256[](0), signaturePubKey);
-    users[msg.sender].signaturePubKey = signaturePubKey;
-    users[msg.sender].keyPackageIndices.push(keyPackageIndex);
-
-    emit UserAdded(msg.sender, signaturePubKey);
-  }
-
-  function getUser(address user) external view returns (UserInfo memory) {
-    return users[user];
-  }
-
-  function addKeyPackage(KeyPackage calldata keyPackage) external {
-    if (keyPackage.data.length == 0) revert MalformedKeyPackage();
-    if (!userExists(msg.sender)) revert UserDoesNotExist();
-
-    keyPackages.push(keyPackage);
-    uint256 keyPackageIndex = keyPackages.length - 1;
-    users[msg.sender].keyPackageIndices.push(keyPackageIndex);
-
-    emit UserKeyPackageAdded(msg.sender, keyPackageIndex);
-  }
-
-  function getAvailableKeyPackage(address user) external view returns (KeyPackage memory) {
-    UserInfo memory userInfo = users[user];
-    uint256 keyPackageIndex = userInfo.keyPackageIndices[userInfo.keyPackageIndices.length - 1];
-    return keyPackages[keyPackageIndex];
-  }
-
-  function getAllKeyPackagesForUser(address user) external view returns (KeyPackage[] memory) {
-    UserInfo memory userInfo = users[user];
-    KeyPackage[] memory userKeyPackages = new KeyPackage[](userInfo.keyPackageIndices.length);
-    for (uint256 i = 0; i < userInfo.keyPackageIndices.length; i++) {
-      userKeyPackages[i] = keyPackages[userInfo.keyPackageIndices[i]];
+    function userExists(address user) public view returns (bool) {
+        return users[user].signaturePubKey.length > 0;
     }
-    return userKeyPackages;
-  }
+
+    function addUser(bytes calldata signaturePubKey, KeyPackage calldata keyPackage) external onlyOwner {
+        if (signaturePubKey.length == 0) revert MalformedUserInfo();
+        if (keyPackage.data.length == 0) revert MalformedKeyPackage();
+        if (userExists(msg.sender)) revert UserAlreadyExists();
+
+        keyPackages.push(keyPackage);
+        uint256 keyPackageIndex = keyPackages.length - 1;
+
+        users[msg.sender] = UserInfo(new uint256[](0), signaturePubKey);
+        users[msg.sender].signaturePubKey = signaturePubKey;
+        users[msg.sender].keyPackageIndices.push(keyPackageIndex);
+
+        emit UserAdded(msg.sender, signaturePubKey);
+    }
+
+    function getUser(address user) external view returns (UserInfo memory) {
+        return users[user];
+    }
+
+    function addKeyPackage(KeyPackage calldata keyPackage) external {
+        if (keyPackage.data.length == 0) revert MalformedKeyPackage();
+        if (!userExists(msg.sender)) revert UserDoesNotExist();
+
+        keyPackages.push(keyPackage);
+        uint256 keyPackageIndex = keyPackages.length - 1;
+        users[msg.sender].keyPackageIndices.push(keyPackageIndex);
+
+        emit UserKeyPackageAdded(msg.sender, keyPackageIndex);
+    }
+
+    function getAvailableKeyPackage(address user) external view returns (KeyPackage memory) {
+        UserInfo memory userInfo = users[user];
+        uint256 keyPackageIndex = userInfo.keyPackageIndices[userInfo.keyPackageIndices.length - 1];
+        return keyPackages[keyPackageIndex];
+    }
+
+    function getAllKeyPackagesForUser(address user) external view returns (KeyPackage[] memory) {
+        UserInfo memory userInfo = users[user];
+        KeyPackage[] memory userKeyPackages = new KeyPackage[](userInfo.keyPackageIndices.length);
+        for (uint256 i = 0; i < userInfo.keyPackageIndices.length; i++) {
+            userKeyPackages[i] = keyPackages[userInfo.keyPackageIndices[i]];
+        }
+        return userKeyPackages;
+    }
 }
--- contracts/test/ScKeystore.t.sol
+++ contracts/test/ScKeystore.t.sol
@@ -8,61 +8,61 @@ import "forge-std/console.sol";
 import "../src/ScKeystore.sol"; // solhint-disable-line
 
 contract ScKeystoreTest is Test {
-  ScKeystore internal s;
-  DeploymentConfig internal deploymentConfig;
-  address internal deployer;
+    ScKeystore internal s;
+    DeploymentConfig internal deploymentConfig;
+    address internal deployer;
 
-  function setUp() public virtual {
-    Deploy deployment = new Deploy();
-    (s, deploymentConfig) = deployment.run(address(this));
-  }
+    function setUp() public virtual {
+        Deploy deployment = new Deploy();
+        (s, deploymentConfig) = deployment.run(address(this));
+    }
 
-  function addUser() internal {
-    KeyPackage memory keyPackage = KeyPackage({ data: new bytes[](1) });
-    s.addUser("0x", keyPackage);
-  }
+    function addUser() internal {
+        KeyPackage memory keyPackage = KeyPackage({ data: new bytes[](1) });
+        s.addUser("0x", keyPackage);
+    }
 
-  function test__owner() public view {
-    assert(s.owner() == address(this));
-  }
+    function test__owner() public view {
+        assert(s.owner() == address(this));
+    }
 
-  function test__userExists__returnsFalse__whenUserDoesNotExist() public view {
-    assert(!s.userExists(address(this)));
-  }
+    function test__userExists__returnsFalse__whenUserDoesNotExist() public view {
+        assert(!s.userExists(address(this)));
+    }
 
-  function test__addUser__reverts__whenUserInfoIsMalformed() public {
-    vm.expectRevert(MalformedUserInfo.selector);
-    s.addUser("", KeyPackage({ data: new bytes[](0) }));
-  }
+    function test__addUser__reverts__whenUserInfoIsMalformed() public {
+        vm.expectRevert(MalformedUserInfo.selector);
+        s.addUser("", KeyPackage({ data: new bytes[](0) }));
+    }
 
-  function test__addUser__reverts__whenUserAlreadyExists() public {
-    addUser();
-    vm.expectRevert(UserAlreadyExists.selector);
-    addUser();
-  }
+    function test__addUser__reverts__whenUserAlreadyExists() public {
+        addUser();
+        vm.expectRevert(UserAlreadyExists.selector);
+        addUser();
+    }
 
-  function test__addUser__addsUser__whenUserInfoIsValid() public {
-    addUser();
-    assert(s.userExists(address(this)));
-  }
+    function test__addUser__addsUser__whenUserInfoIsValid() public {
+        addUser();
+        assert(s.userExists(address(this)));
+    }
 
-  function test__addUser__reverts__whenSenderIsNotOwner() public {
-    vm.prank(address(0));
-    vm.expectRevert();
-    addUser();
-    vm.stopPrank();
-  }
+    function test__addUser__reverts__whenSenderIsNotOwner() public {
+        vm.prank(address(0));
+        vm.expectRevert();
+        addUser();
+        vm.stopPrank();
+    }
 
-  function test__getUser__returnsUserInfo__whenUserExists() public {
-    addUser();
-    UserInfo memory userInfo = s.getUser(address(this));
-    assert(userInfo.signaturePubKey.length == 2);
-    assert(userInfo.keyPackageIndices.length == 1);
-  }
+    function test__getUser__returnsUserInfo__whenUserExists() public {
+        addUser();
+        UserInfo memory userInfo = s.getUser(address(this));
+        assert(userInfo.signaturePubKey.length == 2);
+        assert(userInfo.keyPackageIndices.length == 1);
+    }
 
-  function test__getAllKeyPackagesForUser__returnsKeyPackages__whenUserExists() public {
-    addUser();
-    KeyPackage[] memory keyPackages = s.getAllKeyPackagesForUser(address(this));
-    assert(keyPackages.length == 1);
-  }
+    function test__getAllKeyPackagesForUser__returnsKeyPackages__whenUserExists() public {
+        addUser();
+        KeyPackage[] memory keyPackages = s.getAllKeyPackagesForUser(address(this));
+        assert(keyPackages.length == 1);
+    }
 }
