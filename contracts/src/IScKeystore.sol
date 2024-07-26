f819a5849361502c2b37b2d1ab4da388fd96fe6b
Aaryamann Challani <43716372+rymnc@users.noreply.github.com>
2024-07-26 23:08:41 +0530

    chore(contract): remove keypackage refs, regen bindings (#24)

--- contracts/src/IScKeystore.sol
+++ contracts/src/IScKeystore.sol
@@ -1,24 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-struct KeyPackage {
-    // store serialized onchain
-    bytes[] data;
-}
-
 struct UserInfo {
-    uint256[] keyPackageIndices;
     bytes signaturePubKey;
 }
 
 interface IScKeystore {
     function userExists(address user) external view returns (bool);
 
-    function addUser(address user, bytes calldata signaturePubKey, KeyPackage calldata keyPackage) external;
+    function addUser(address user, bytes calldata signaturePubKey) external;
 
     function getUser(address user) external view returns (UserInfo memory);
-
-    function addKeyPackage(KeyPackage calldata) external;
-
-    function getAvailableKeyPackage(address user) external view returns (KeyPackage memory);
 }
--- contracts/src/ScKeystore.sol
+++ contracts/src/ScKeystore.sol
@@ -2,19 +2,16 @@
 pragma solidity 0.8.24;
 
 import { Ownable } from "Openzeppelin/access/Ownable.sol";
-import { IScKeystore, UserInfo, KeyPackage } from "./IScKeystore.sol";
+import { IScKeystore, UserInfo } from "./IScKeystore.sol";
 
 error UserAlreadyExists();
-error MalformedKeyPackage();
 error MalformedUserInfo();
 error UserDoesNotExist();
 
 contract ScKeystore is Ownable, IScKeystore {
     event UserAdded(address user, bytes signaturePubKey);
-    event UserKeyPackageAdded(address indexed user, uint256 index);
 
     mapping(address user => UserInfo userInfo) private users;
-    KeyPackage[] private keyPackages;
 
     constructor(address initialOwner) Ownable(initialOwner) { }
 
@@ -22,48 +19,16 @@ contract ScKeystore is Ownable, IScKeystore {
         return users[user].signaturePubKey.length > 0;
     }
 
-    function addUser(address user, bytes calldata signaturePubKey, KeyPackage calldata keyPackage) external onlyOwner {
+    function addUser(address user, bytes calldata signaturePubKey) external onlyOwner {
         if (signaturePubKey.length == 0) revert MalformedUserInfo();
-        if (keyPackage.data.length == 0) revert MalformedKeyPackage();
         if (userExists(user)) revert UserAlreadyExists();
 
-        keyPackages.push(keyPackage);
-        uint256 keyPackageIndex = keyPackages.length - 1;
-
-        users[user] = UserInfo(new uint256[](0), signaturePubKey);
-        users[user].signaturePubKey = signaturePubKey;
-        users[user].keyPackageIndices.push(keyPackageIndex);
+        users[user] = UserInfo(signaturePubKey);
 
         emit UserAdded(user, signaturePubKey);
     }
 
     function getUser(address user) external view returns (UserInfo memory) {
         return users[user];
-    }
-
-    function addKeyPackage(KeyPackage calldata keyPackage) external {
-        if (keyPackage.data.length == 0) revert MalformedKeyPackage();
-        if (!userExists(msg.sender)) revert UserDoesNotExist();
-
-        keyPackages.push(keyPackage);
-        uint256 keyPackageIndex = keyPackages.length - 1;
-        users[msg.sender].keyPackageIndices.push(keyPackageIndex);
-
-        emit UserKeyPackageAdded(msg.sender, keyPackageIndex);
-    }
-
-    function getAvailableKeyPackage(address user) external view returns (KeyPackage memory) {
-        UserInfo memory userInfo = users[user];
-        uint256 keyPackageIndex = userInfo.keyPackageIndices[userInfo.keyPackageIndices.length - 1];
-        return keyPackages[keyPackageIndex];
-    }
-
-    function getAllKeyPackagesForUser(address user) external view returns (KeyPackage[] memory) {
-        UserInfo memory userInfo = users[user];
-        KeyPackage[] memory userKeyPackages = new KeyPackage[](userInfo.keyPackageIndices.length);
-        for (uint256 i = 0; i < userInfo.keyPackageIndices.length; i++) {
-            userKeyPackages[i] = keyPackages[userInfo.keyPackageIndices[i]];
-        }
-        return userKeyPackages;
     }
 }
