eb6f532a815c226df2b9739ca6e4b2315230bcc3
seemenkina <seemenkina@gmail.com>
2026-04-30 17:37:29 +0700

    refactor(ds): hoist WakuFfiError import to top of error.rs

--- src/ds/error.rs
+++ src/ds/error.rs
@@ -1,17 +1,20 @@
+#[cfg(feature = "waku")]
+use crate::ds::waku::wrapper::WakuFfiError;
+
 /// Errors originating from the delivery service layer.
 #[derive(Debug, thiserror::Error)]
 pub enum DeliveryServiceError {
     #[cfg(feature = "waku")]
     #[error("Waku publish failed: {0}")]
-    WakuPublish(#[source] crate::ds::waku::wrapper::WakuFfiError),
+    WakuPublish(#[source] WakuFfiError),
 
     #[cfg(feature = "waku")]
     #[error("Waku node startup failed: {0}")]
-    WakuStartup(#[source] crate::ds::waku::wrapper::WakuFfiError),
+    WakuStartup(#[source] WakuFfiError),
 
     #[cfg(feature = "waku")]
     #[error("Waku connect peer failed: {0}")]
-    WakuConnectPeer(#[source] crate::ds::waku::wrapper::WakuFfiError),
+    WakuConnectPeer(#[source] WakuFfiError),
 
     #[error("An unknown error occurred: {0}")]
     Other(anyhow::Error),
